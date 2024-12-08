993e8cf679ed79f84d796169dca540c7a2da652f
julfikar@LT-SOFTWAREDEV3 <skjulfikar323@gmail.com>
2024-12-08 23:33:05 +0530

    Initially i have update the application

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using EmployeeDetailsApp.Models;
@@ -59,17 +60,71 @@ namespace EmployeeDetailsApp.Controllers
             if (ModelState.IsValid)
             {
                 using (var db = new ApplicationDbContext())
-                {
-                    var checkData = db.Teachers.Find(data.EmpId);
-                    if (checkData != null)
+                {                    
+                    var existingTeacher = db.Teachers.Find(data.EmpId); // Fetch the existing entity
+                    if (existingTeacher != null)
                     {
-                        db.Entry(data).State = EntityState.Modified;
-                        db.SaveChanges();
+                        existingTeacher.Name = data.Name;
+                        existingTeacher.Age = data.Age;
+                        existingTeacher.EmailId = data.EmailId;
+                        existingTeacher.JoiningDate = data.JoiningDate;
+
+                        db.SaveChanges(); // Save changes
+                        return RedirectToAction("Index");
                     }
-                    return RedirectToAction("index");
                 }
             }
             return View();
         }
+    
+        [HttpGet]
+        public ActionResult GetTeachers(int? id)
+        {
+            using(var db = new ApplicationDbContext())
+            {
+                var data = db.Teachers.Find(id);
+                if (data == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(data);
+            }            
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest); // Return HTTP 400 if id is not provided
+            }
+
+            using (var db = new ApplicationDbContext())
+            {
+                var teacher = db.Teachers.Find(id);
+                if (teacher == null)
+                {
+                    return HttpNotFound(); // Return HTTP 404 if the teacher is not found
+                }
+                return View(teacher); // Pass the teacher to the confirmation view
+            }
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken] 
+        public ActionResult DeleteConfirmed(int id)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var teacher = db.Teachers.Find(id);
+                if (teacher != null)
+                {
+                    db.Teachers.Remove(teacher); 
+                    db.SaveChanges(); 
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
